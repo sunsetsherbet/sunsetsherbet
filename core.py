24d05c2ab910d5835494e4733ab5a1b486f95fe0
Hunter <95716964+sunsetsherbet@users.noreply.github.com>
2022-04-19 15:05:41 -0500

    Create core.py

---
+++ core.py
@@ -0,0 +1,19 @@
+def multi(a, b):
+    mom = float(a) * float(b)
+    print(str(a) + ' * ' + str(b) + ' = ' + str(mom))
+
+  
+def divis(d, f):
+    dom = float(d) / float(f)
+    print(str(d) + "/" + str(f) + " = " str(dom)
+
+        
+def addit(e, j):
+    aom = float(e) + float(j)
+    print(str(e) + ' + ' + str(j) + ' = ' + str(aom))
+        
+        
+def subtra(s, k):
+    som = float(s) - float(k)
+    print(str(s) + " - " + str(k) + " = " + str(som))
+        
